c7fa2916f41a30abc53c99afbd83b510299e588a
vickodion <vickodion2016@gmail.com>
2024-02-09 09:20:22 +0100

    set up

---
+++ school management system/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1 className='text-orange-700 font-bold text-2xl'>hello world</h1>
+    </div>
+  )
+}
+
+export default App
